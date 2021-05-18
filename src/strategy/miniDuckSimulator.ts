f6a3d9acb7930aef6e990d74b2481dc9c398c7bb
dayakar <hdayakar@gmail.com>
2021-05-18 20:30:23 +0530

    initial commit

---
+++ src/strategy/miniDuckSimulator.ts
@@ -0,0 +1,22 @@
+class MiniDuckSimulator {
+ 
+	public main(args: string[]): void {
+		console.log("test");
+ 
+		// MallardDuck	mallard = new MallardDuck();
+		// RubberDuck	rubberDuckie = new RubberDuck();
+		// DecoyDuck	decoy = new DecoyDuck();
+ 
+		// ModelDuck	model = new ModelDuck();
+
+		// mallard.performQuack();
+		// rubberDuckie.performQuack();
+		// decoy.performQuack();
+   
+		// model.performFly();	
+		// model.setFlyBehavior(new FlyRocketPowered());
+		// model.performFly();
+	}
+}
+
+console.log("test1");
